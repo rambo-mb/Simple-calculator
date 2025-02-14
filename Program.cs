715193f76993eef18693d501d1330dfb685b4678
Izzatjon Qodirov <qodirov.izzatjon.rambo.mb@gmail.com>
2025-02-14 22:19:21 +0500

    Ready

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Oddiy kalkulyatorga xush kelibsiz!");
+Console.Write("Kiriting: ");
+string input = Console.ReadLine();
+
+if (input.IndexOf("+") != -1)
+{
+    int a = Convert.ToInt32(input.Substring(0, input.IndexOf("+")));
+    int b = Convert.ToInt32(input.Substring(input.IndexOf("+")+1));
+    Console.WriteLine($"Natija: {a + b}");
+}
+else if (input.IndexOf("-") != -1)
+{
+    int a = Convert.ToInt32(input.Substring(0, input.IndexOf("-")));
+    int b = Convert.ToInt32(input.Substring(input.IndexOf("-")+1));
+    Console.WriteLine($"Natija: {a - b}");
+}
+else if (input.IndexOf("*") != -1)
+{
+    int a = Convert.ToInt32(input.Substring(0, input.IndexOf("*")));
+    int b = Convert.ToInt32(input.Substring(input.IndexOf("*")+1));
+    Console.WriteLine($"Natija: {a * b}");
+}
+else if (input.IndexOf("/") != -1)
+{
+    int a = Convert.ToInt32(input.Substring(0, input.IndexOf("/")));
+    int b = Convert.ToInt32(input.Substring(input.IndexOf("/")+1));
+    Console.WriteLine($"Natija: {a / b}");
+}
+else
+{
+    Console.WriteLine("Noto'g'ri formatda kiritdingiz!");
+}
